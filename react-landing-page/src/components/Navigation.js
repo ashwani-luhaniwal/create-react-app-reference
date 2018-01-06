2a91f1aecca4c9062d47c33932f05f8bb1a49bc1
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-06 00:01:13 -0800

    Navigation component

---
+++ react-landing-page/src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * Navigation component
+ */
+export default class Navigation extends React.Component {
+    render() {
+        return (
+            <div id="navigation" className="Navigation">
+                <nav>
+                    <ul>
+                        <li>Browse</li>
+                        <li>My list</li>
+                        <li>Top picks</li>
+                        <li>Recent</li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
