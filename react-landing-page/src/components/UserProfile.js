fe66545178f895632afa33e3b824221a54eed73a
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-06 00:02:08 -0800

    User Profile component

---
+++ react-landing-page/src/components/UserProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * User Profile component
+ */
+export default class UserProfile extends React.Component {
+    render() {
+        return (
+            <div className="UserProfile">
+                <div className="User">
+                    <div className="name">Ashwani Luhaniwal</div>
+                    <div className="image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg" alt="profile" /></div>
+                </div>
+            </div>
+        )
+    }
+}
