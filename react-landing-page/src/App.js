e3c19b7d1bc31cec81ff92cc58d22f52030f26fc
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-06 00:59:25 -0800

    Main app component

--- react-landing-page/src/App.js
+++ react-landing-page/src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Logo from './components/Logo';
+import Navigation from './components/Navigation';
+import UserProfile from './components/UserProfile';
+import Hero from './components/Hero/Hero';
+import TitleList from './components/TitleList/TitleList';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+            searchUrl: ''
+        };
+        const apiKey = '87dfa1c669eea853da609d4968d294be';
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleKeyUp(e) {
+        if (e.key === 'Enter' && this.state.searchTerm !== '') {
+            let searchUrl = 'search/multi?query=' + this.state.searchTerm + '&api_key=' + this.apiKey;
+            this.setState({ searchUrl: searchUrl });
+        }
+    }
+    handleChange(e) {
+        this.setState({ searchTerm: e.target.value });
+    }
+    render() {
+        return (
+            <div>
+                <header className="Header">
+                    <Logo />
+                    <Navigation />
+                    <div id="search" className="Search">
+                        <input onKeyUp={this.handleKeyUp} onChange={this.handleChange} type="search" placeholder="Search for a titile..." value={this.state.searchTerm} />
+                    </div>
+                    <UserProfile />
+                </header>
+                <Hero />
+                <TitleList title="Search Results" url={this.state.searchUrl} />
+                <TitleList title="Top TV picks for Ashwani" url='discover/tv?sort_by=popularity.desc&page=1' />
+                <TitleList title="Trending now" url="discover/movie?sort_by=popularity.desc&page=1" />
+                <TitleList title="Most watched in Horror" url="genre/27/movies?sort_by=popularity.desc&page=1" />
+                <TitleList title="Sci-Fi greats" url="genre/878/movies?sort_by=popularity.desc&page=1" />
+                <TitleList title="Comedy magic" url="genre/35/movies?sort_by=popularity.desc&page=1" />
+            </div>
+        );
+    }
 }
-
-export default App;
