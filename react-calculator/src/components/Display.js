663d31d8c521b6cf5b96edad6380e32977f19da2
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-07 09:20:27 -0800

    Display component with description

--- react-calculator/src/components/Display.js
+++ react-calculator/src/components/Display.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 /**
  * Display component
+ * ------------------
+ * We take all values from this.state.operations and concatenate them into a string using the
+ * 'join' method. Our render method returns the string wrapped in a <div>
  */
 export default class Display extends React.Component {
     render() {
