33fee4c27c5fad9b08b1cc964784c30b25d56262
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-06 23:49:44 -0800

    Main app component

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,21 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import update from 'react-addons-update';
+import math from 'mathjs';
+import Display from './components/Display';
+import Buttons from './components/Buttons';
+import Button from './components/Button';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            operations: []
+        };
+        this.calculateOperations = this.calculateOperations.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    calculateOperations() {
+        let result = this.state.operations.join('');
+        if (result) {
+            result = String(math.eval(result));
+            this.setState({ operations: [result] });
+        }
+    }
+    handleClick(e) {
+        let value = e.target.getAttribute('data-value');
+        switch (value) {
+            case 'clear':
+                this.setState({ operations: [] });
+                break;
+            case 'equal':
+                this.calculateOperations();
+                break;
+            default:
+                let newOperations = update(this.state.operations, {$push: [value]});
+                this.setState({ operations: newOperations });
+                break;
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <Display data={this.state.operations} />
+                <Buttons>
+                    <Button onClick={this.handleClick} label="C" value="clear" />
+                    <Button onClick={this.handleClick} label="7" value="7" />
+                    <Button onClick={this.handleClick} label="4" value="4" />
+                    <Button onClick={this.handleClick} label="1" value="1" />
+                    <Button onClick={this.handleClick} label="0" value="0" />
 
-export default App;
+                    <Button onClick={this.handleClick} label="/" value="/" />
+                    <Button onClick={this.handleClick} label="8" value="8" />
+                    <Button onClick={this.handleClick} label="5" value="5" />
+                    <Button onClick={this.handleClick} label="2" value="2" />
+                    <Button onClick={this.handleClick} label="." value="." />
+
+                    <Button onClick={this.handleClick} label="x" value="*" />
+                    <Button onClick={this.handleClick} label="9" value="9" />
+                    <Button onClick={this.handleClick} label="6" value="6" />
+                    <Button onClick={this.handleClick} label="3" value="3" />
+                    <Button label="" value="" />
+
+                    <Button onClick={this.handleClick} label="-" value="-" />
+                    <Button onClick={this.handleClick} label="+" size="2" value="+" />
+                    <Button onClick={this.handleClick} label="=" size="2" value="equal" />
+                </Buttons>
+            </div>
+        );
+    }
+}
