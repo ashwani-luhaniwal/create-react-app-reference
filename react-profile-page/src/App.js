ec880e89976f6c7e905998a0a710c3bbe4053fbd
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-08 11:09:28 -0800

    Main component for React Profile Page App

--- react-profile-page/src/App.js
+++ react-profile-page/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Image from './components/Image';
+import Profile from './components/Profile';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+/**
+ * Main App component
+ */
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            person: {
+                name: 'Ashwani Luhaniwal',
+                biography: '26 year old Designer/Developer living in Stockholm. Originally from India. Love to make stuff.',
+            },
+            image: 'http://static1.squarespace.com/static/55acc005e4b098e615cd80e2/t/57b057398419c2c454f09924/1471025851733/',
+            quote: {
+                content: 'Beautiful things don\'t ask for attention',
+                source: 'The Secret Life of Walter Mitty'
+            }
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <Image src={this.state.image} />
+                <Profile person={this.state.person} quote={this.state.quote} />
+            </div>
+        );
+    }
 }
-
-export default App;
