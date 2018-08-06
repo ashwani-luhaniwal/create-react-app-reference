f580e46483c8630912ea0f79258798094e6cce05
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-08-06 09:41:55 -0700

    Require auth utils

---
+++ medium-clone/src/utils/requireAuth.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+export default function (Component) {
+  class Authenticate extends Component {
+    componentWillMount() {
+      if (!this.props.isAuth) {
+        this.context.router.history.push('/')
+      }
+    }
+    render() {
+      return(
+        <Component {...this.props} />
+      )
+    }
+  }
+
+  Authenticate.contextType = {
+    router: PropTypes.object.isRequired
+  }
+
+  const mapStateToProps = state => {
+    return {
+      isAuth: state.authUser.isAuth
+    }
+  }
+
+  return connect(mapStateToProps)(Authenticate)
+}
